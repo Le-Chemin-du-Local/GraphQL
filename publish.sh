5b504cfa2286ea071ad1fc02adaa302f07df0c02
Feldrise <admin@feldrise.com>
2022-04-27 19:12:09 +0200

    💚 Mise à jour des outils pour Docker

--- publish.sh
+++ publish.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build --build-arg PORT_NUMBER=3000 --build-arg ENV=production -t chemindulocal/api:latest .
+docker build --build-arg PORT_NUMBER=3000 --build-arg ENV=production -t chemindulocal/chemindulocal-api:latest .
+docker tag chemindulocal/chemindulocal-api:latest chemindulocal/chemindulocal-api:$1
+docker push chemindulocal/chemindulocal-api:latest
+docker push chemindulocal/chemindulocal-api:$1
